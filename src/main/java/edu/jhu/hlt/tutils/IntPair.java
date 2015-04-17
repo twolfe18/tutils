ac5a8275088ec39872c58ff75d057fd65af16943
Travis Wolfe <twolfe18@gmail.com>
2015-04-17 10:22:34 -0400

    Added (immutable) IntPair

---
+++ src/main/java/edu/jhu/hlt/tutils/IntPair.java
@@ -0,0 +1,30 @@
+package edu.jhu.hlt.tutils;
+
+/**
+ * Immutable.
+ * @author travis
+ */
+public class IntPair {
+
+  public final int first, second;
+  public final int hash;
+
+  public IntPair(int first, int second) {
+    this.first = first;
+    this.second = second;
+    this.hash = Integer.reverse(first) ^ second;
+  }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  public boolean equals(Object other) {
+    if (other instanceof IntPair) {
+      IntPair x = (IntPair) other;
+      return first == x.first && second == x.second;
+    }
+    return false;
+  }
+}
