9d13bcf906a27014fdafe708801d44c14c0c19f5
Travis Wolfe <twolfe18@gmail.com>
2015-07-02 10:14:26 -0400

    added IntTrip to represent triples of integers

---
+++ src/main/java/edu/jhu/hlt/tutils/IntTrip.java
@@ -0,0 +1,33 @@
+package edu.jhu.hlt.tutils;
+
+public class IntTrip {
+
+  public final int first, second, third;
+  public final int hash;
+
+  public IntTrip(int first, int second, int third) {
+    this.first = first;
+    this.second = second;
+    this.third = third;
+    this.hash = first + 31 * (second + 31 * third);
+  }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  public boolean equals(Object other) {
+    if (other instanceof IntPair) {
+      IntTrip x = (IntTrip) other;
+      return first == x.first && second == x.second && third == x.third;
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + first + ", " + second + ", " + third + ")";
+  }
+
+}
