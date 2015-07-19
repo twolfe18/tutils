d8d6a10299d138ed5692dea94cbc463572fecd6b
Travis Wolfe <twolfe18@gmail.com>
2015-07-19 18:28:58 -0400

    PosUtil no longer packs into a long, uses BitSet

--- src/main/java/edu/jhu/hlt/tutils/ling/PosUtil.java
+++ src/main/java/edu/jhu/hlt/tutils/ling/PosUtil.java
@@ -1,58 +1,65 @@
 package edu.jhu.hlt.tutils.ling;
 
+import java.io.Serializable;
+import java.util.BitSet;
+
 import edu.jhu.hlt.tutils.MultiAlphabet;
 
 
-public class PosUtil {
+public class PosUtil implements Serializable {
+  private static final long serialVersionUID = -5172427447025542500L;
 
-  public final long VERBS, NOUNS, PROPER_NOUNS;
+  public final BitSet verbs, nouns, properNouns, pronouns;
   private MultiAlphabet alph;
 
   public PosUtil(MultiAlphabet alph) {
     this.alph = alph;
-    VERBS = 0
-        | (1l << alph.pos("VA"))      // Chinese
-        | (1l << alph.pos("VC"))      // Chinese
-        | (1l << alph.pos("VE"))      // Chinese
-        | (1l << alph.pos("VV"))      // Chinese
-        | (1l << alph.pos("VBD"))
-        | (1l << alph.pos("VBG"))
-        | (1l << alph.pos("VBN"))
-        | (1l << alph.pos("VBP"))
-        | (1l << alph.pos("VBZ"));
-    // etc
-    NOUNS = 0
-        | (1l << alph.pos("NR"))      // Chinese
-        | (1l << alph.pos("NT"))      // Chinese
-        | (1l << alph.pos("NN"))
-        | (1l << alph.pos("NNS"))
-        | (1l << alph.pos("NNP"))
-        | (1l << alph.pos("NNPS"));
-    // etc
-    PROPER_NOUNS = 0
-        | (1l << alph.pos("NR"))      // Chinese
-        | (1l << alph.pos("NNP"))
-        | (1l << alph.pos("NNPS"));
-  }
-
-  public boolean matches(long mask, int pos) {
-    assert pos < 64;
-    return (mask & (1l << pos)) != 0;
+    this.verbs = new BitSet();
+    verbs.set(alph.pos("VA"));
+    verbs.set(alph.pos("VC"));
+    verbs.set(alph.pos("VE"));
+    verbs.set(alph.pos("VV"));
+    verbs.set(alph.pos("VBD"));
+    verbs.set(alph.pos("VBG"));
+    verbs.set(alph.pos("VBN"));
+    verbs.set(alph.pos("VBP"));
+    verbs.set(alph.pos("VBZ"));
+    this.nouns = new BitSet();
+    nouns.set(alph.pos("NR"));
+    nouns.set(alph.pos("NT"));
+    nouns.set(alph.pos("NN"));
+    nouns.set(alph.pos("NNS"));
+    nouns.set(alph.pos("NNP"));
+    nouns.set(alph.pos("NNPS"));
+    this.properNouns = new BitSet();
+    properNouns.set(alph.pos("NR"));
+    properNouns.set(alph.pos("NNP"));
+    properNouns.set(alph.pos("NNPS"));
+    this.pronouns = new BitSet();
+    pronouns.set(alph.pos("PN"));
+    pronouns.set(alph.pos("WP"));
+    pronouns.set(alph.pos("WP$"));
+    pronouns.set(alph.pos("PRP"));
+    pronouns.set(alph.pos("PRP$"));
   }
 
   public boolean isNoun(int pos) {
-    return matches(NOUNS, pos);
+    return nouns.get(pos);
   }
 
   public boolean isProperNoun(int pos) {
-    return matches(PROPER_NOUNS, pos);
+    return properNouns.get(pos);
+  }
+
+  public boolean isProNoun(int pos) {
+    return pronouns.get(pos);
   }
 
   public boolean isVerb(int pos) {
-    return matches(VERBS, pos);
+    return verbs.get(pos);
   }
 
-  public int numPosTags() {
-    return alph.numPos();
+  public MultiAlphabet getAlphabet() {
+    return alph;
   }
 }
