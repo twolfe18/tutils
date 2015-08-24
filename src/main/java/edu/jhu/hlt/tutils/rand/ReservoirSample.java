19802e4dec9bcd8ae541fa2c6896d7a52a11f696
Travis Wolfe <twolfe18@gmail.com>
2015-08-24 14:28:13 -0400

    fixed bug in ReservoirSample

--- src/main/java/edu/jhu/hlt/tutils/rand/ReservoirSample.java
+++ src/main/java/edu/jhu/hlt/tutils/rand/ReservoirSample.java
@@ -1,32 +1,30 @@
 package edu.jhu.hlt.tutils.rand;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 public class ReservoirSample {
 
-  public static <T> T[] sample(Iterator<T> stream, int k, Random rand) {
-    @SuppressWarnings("unchecked")
-    T[] res = (T[]) new Object[k];
+  public static <T> List<T> sample(Iterator<T> stream, int k, Random rand) {
+    List<T> res = new ArrayList<>();
     int taken = 0;
     while (stream.hasNext()) {
       T n = stream.next();
       taken++;
       if (taken <= k)
-        res[taken - 1] = n;
+        res.add(n);
       else {
         int i = rand.nextInt(taken);
         if (i < k)
-          res[i] = n;
+          res.set(i, n);
       }
     }
-    if (taken < k)
-      return Arrays.copyOf(res, taken);
     return res;
   }
 
   public static <T> T sampleOne(Iterator<T> stream, Random rand) {
-    return sample(stream, 1, rand)[0];
+    return sample(stream, 1, rand).get(0);
   }
 }
