0aadc699b969d7fee9d8bf31a74e3ed3cebc797a
Travis Wolfe <twolfe18@gmail.com>
2015-08-26 12:28:14 -0400

    ReservoirSample now supports more types

--- src/main/java/edu/jhu/hlt/tutils/rand/ReservoirSample.java
+++ src/main/java/edu/jhu/hlt/tutils/rand/ReservoirSample.java
@@ -7,6 +7,14 @@ import java.util.Random;
 
 public class ReservoirSample {
 
+  /**
+   * NOTE: If you're not using an iterator, you're probably not getting the full
+   * benefit of a reservoir sample.
+   */
+  public static <T> List<T> sample(Iterable<T> stream, int k, Random rand) {
+    return sample(stream.iterator(), k, rand);
+  }
+
   public static <T> List<T> sample(Iterator<T> stream, int k, Random rand) {
     List<T> res = new ArrayList<>();
     int taken = 0;
@@ -22,6 +30,14 @@ public class ReservoirSample {
       }
     }
     return res;
+  }
+
+  /**
+   * NOTE: If you're not using an iterator, you're probably not getting the full
+   * benefit of a reservoir sample.
+   */
+  public static <T> T sampleOne(Iterable<T> stream, Random rand) {
+    return sample(stream, 1, rand).get(0);
   }
 
   public static <T> T sampleOne(Iterator<T> stream, Random rand) {
