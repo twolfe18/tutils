473897235138ddc0a89740941e360b82ad94b96a
Travis Wolfe <twolfe18@gmail.com>
2016-04-22 11:46:23 -0400

    Timer now produces time (between start and stop) histograms with log-sized buckets

--- src/main/java/edu/jhu/hlt/tutils/Timer.java
+++ src/main/java/edu/jhu/hlt/tutils/Timer.java
@@ -3,12 +3,65 @@ package edu.jhu.hlt.tutils;
 public class Timer {
   private String id;
   private int count;
-  private long time;
+  private long totalTime;
   private long lastStart = -1;
   public int printIterval;
 
   public boolean ignoreFirstTime;
   private long firstTime;
+
+  // timeHistCounts[i] = number of timer events which took between t(i) and t(i+1)
+  // milliseconds, where t(i) = timeHistCoef * 2^i
+  private int[] timeHistCounts = new int[20];
+  private long timeHistCoef = 4;   // first bucket: <8 ms, last bucket: >4.2 minutes
+  public boolean showTimeHistInToString = false;
+  private long timeUpperBound(int i) {
+    if (i < 0 || i >= timeHistCounts.length)
+      throw new IllegalArgumentException();
+    return timeHistCoef * (1L << i);
+  }
+  private void updateHist(long ms) {
+    assert ms < Integer.MAX_VALUE;
+    for (int i = 0; i < timeHistCounts.length; i++) {
+      long ub = timeUpperBound(i);
+      if (ms < ub || i == timeHistCounts.length-1) {
+        timeHistCounts[i]++;
+        return;
+      }
+    }
+  }
+  private static String timeStr(long ms) {
+    double sec = ms / 1000d;
+    double min = sec / 60d;
+    if (min > 1)
+      return String.format("%.1f min", min);
+    if (sec > 1)
+      return String.format("%.1f sec", sec);
+    return ms + " ms";
+  }
+  public String getTimeHist() {
+    int firstNz = -1;
+    int lastNz = -1;
+    for (int i = 0; i < timeHistCounts.length; i++) {
+      if (timeHistCounts[i] > 0) {
+        if (firstNz < 0)
+          firstNz = i;
+        lastNz = i;
+      }
+    }
+    if (lastNz < 0)
+      return "[no times available]";
+    StringBuilder sb = new StringBuilder();
+    for (int i = firstNz; i < lastNz; i++) {
+      if (i == firstNz)
+        sb.append("[<numEvents> below <time> ");
+      else
+        sb.append(' ');
+      sb.append(timeHistCounts[i] + " " + timeStr(timeUpperBound(i)));
+    }
+    sb.append(']');
+    return sb.toString();
+  }
 
   public Timer() {
     this(null, -1, false);
@@ -58,8 +111,9 @@ public class Timer {
     long t = System.currentTimeMillis() - lastStart;
     if(count == 0)
       firstTime = t;
-    time += t;
+    totalTime += t;
     count++;
+    updateHist(t);
     if(printIterval > 0 && count % printIterval == 0)
       System.out.println(this);
     return t;
@@ -75,24 +129,29 @@ public class Timer {
 
   public String toString() {
     double spc = secPerCall();
+    String spcUnit = "sec/call";
     if (spc < 0.001) {
-      return String.format("<Timer %s %.2f sec and %d calls total, %.3f ms/call>",
-          id, totalTimeInSeconds(), count, spc * 1000);
+      spc *= 1000;
+      spcUnit = "ms/call";
     }
-    return String.format("<Timer %s %.2f sec and %d calls total, %.3f sec/call>",
-        id, totalTimeInSeconds(), count, spc);
+    String timeHist = "";
+    if (showTimeHistInToString) {
+      timeHist = " hist=" + getTimeHist();
+    }
+    return String.format("<Timer %s %.2f sec and %d calls total, %.3f %s%s>",
+        id, totalTimeInSeconds(), count, spc, spcUnit, timeHist);
   }
 
   public double countsPerMSec() {
     if(count > 1)
-      return (count - 1d) / (time - firstTime);
-    return ((double) count) / time;
+      return (count - 1d) / (totalTime - firstTime);
+    return ((double) count) / totalTime;
   }
 
   public double secPerCall() {
     if(count > 1)
-      return ((time - firstTime)/1000d) / (count-1);
-    return (time/1000d) / count;
+      return ((totalTime - firstTime)/1000d) / (count-1);
+    return (totalTime/1000d) / count;
   }
 
   public double minutesUntil(int iterations) {
@@ -106,8 +165,8 @@ public class Timer {
     return count;
   }
 
-  public long totalTimeInMilliseconds() { return time; }
-  public double totalTimeInSeconds() { return time / 1000d; }
+  public long totalTimeInMilliseconds() { return totalTime; }
+  public double totalTimeInSeconds() { return totalTime / 1000d; }
 
   public static final class NoOp extends Timer {
     public NoOp(String id) { super(id); }
