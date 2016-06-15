d4928816e523f2d1a4889be9f91c169e2b85a154
Travis Wolfe <twolfe18@gmail.com>
2016-06-15 00:15:21 -0400

    Hash has main method for computing various hash functions on string files

--- src/main/java/edu/jhu/hlt/tutils/hash/Hash.java
+++ src/main/java/edu/jhu/hlt/tutils/hash/Hash.java
@@ -1,11 +1,21 @@
 package edu.jhu.hlt.tutils.hash;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
 import java.security.MessageDigest;
+
+import edu.jhu.hlt.tutils.ExperimentProperties;
+import edu.jhu.hlt.tutils.FileUtil;
+import edu.jhu.hlt.tutils.Log;
+import edu.jhu.hlt.tutils.TimeMarker;
 
 public class Hash {
 
   public static int hash(String s) {
+    if (s == null)
+      return 0;
     return hash(s, 0, s.length());
   }
   public static int hash(String s, int start, int end) {
@@ -115,18 +125,72 @@ public class Hash {
   }
 
   /** A slow but decent hash for strings */
-  public static int sha256(String s) {
+  public static long sha256(String s) {
     try {
       MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
       messageDigest.update(s.getBytes());
-      String e = new String(messageDigest.digest());
-      return e.hashCode();
+      byte[] b = messageDigest.digest();
+//      String e = new String(b);
+//      return e.hashCode();
+      long h = b[0];
+      h = (h << 8) ^ b[1];
+      h = (h << 8) ^ b[2];
+      h = (h << 8) ^ b[3];
+      h = (h << 8) ^ b[4];
+      h = (h << 8) ^ b[5];
+      h = (h << 8) ^ b[6];
+      h = (h << 8) ^ b[7];
+      return h;
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  public static int fileName(File f) {
+  public static long fileName(File f) {
     return sha256(f.getPath());
+  }
+
+  /**
+   * Reads lines from an input file, hashes them, reports the original and
+   * the hash to an output file, and logs the time it took.
+   */
+  public static void main(String[] args) throws IOException {
+    ExperimentProperties config = ExperimentProperties.init(args);
+    File i = config.getExistingFile("input");
+
+    if (config.containsKey("sha256Output")) {
+      File sha256Output = config.getFile("sha256Output");
+      TimeMarker t = new TimeMarker();
+      int n = 0;
+      try (BufferedReader r = FileUtil.getReader(i);
+          BufferedWriter w = FileUtil.getWriter(sha256Output)) {
+        for (String line = r.readLine(); line != null; line = r.readLine()) {
+          long sha256 = sha256(line);
+          w.write(Long.toHexString(sha256) + "\t" + line);
+          w.newLine();
+          if (t.enoughTimePassed(15))
+            System.out.println("[sha256] converted " + n + " lines in " + t.secondsSinceFirstMark());
+        }
+      }
+      Log.info("n=" + n + " sha256Time=" + t.secondsSinceFirstMark() + " sha256AvgTime=" + t.secondsPerMark());
+    }
+
+    if (config.containsKey("jenkinsOutput")) {
+      File jenkinsOutput = config.getFile("jenkinsOutput");
+      TimeMarker t = new TimeMarker();
+      int n = 0;
+      try (BufferedReader r = FileUtil.getReader(i);
+          BufferedWriter w = FileUtil.getWriter(jenkinsOutput)) {
+        for (String line = r.readLine(); line != null; line = r.readLine()) {
+          n++;
+          long jenkins = hash(line);
+          w.write(Long.toHexString(jenkins) + "\t" + line);
+          w.newLine();
+          if (t.enoughTimePassed(15))
+            System.out.println("[jenkins] converted " + n + " lines in " + t.secondsSinceFirstMark());
+        }
+      }
+      Log.info("n=" + n + " jenkinsTime=" + t.secondsSinceFirstMark() + " jenkinsAvgTime=" + t.secondsPerMark());
+    }
   }
 }
