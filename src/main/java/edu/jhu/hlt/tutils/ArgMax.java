1350e611b933d9e0b8773162ff1fa21227e379ba
Travis Wolfe <twolfe18@gmail.com>
2016-07-27 19:24:52 -0700

    ArgMax improvements

--- src/main/java/edu/jhu/hlt/tutils/ArgMax.java
+++ src/main/java/edu/jhu/hlt/tutils/ArgMax.java
@@ -6,17 +6,35 @@ public class ArgMax<T> implements Serializable {
   private static final long serialVersionUID = 5760960133608840784L;
 
   private T bestItem;
-  private double bestScore;
+  private double bestScore = Double.NaN;
+  private int bestIndex = -1;
   private int offers = 0;
 
-  public void offer(T item, double score) {
-    if (item == null || Double.isInfinite(score) || Double.isNaN(score))
+  @Override
+  public String toString() {
+    return String.format("(ArgMax n=%d bestIndex=%d bestScore=%+2f bestItem=%s)",
+        offers, bestIndex, bestScore, bestItem);
+  }
+
+  /**
+   * Returns true if this is the new argmax.
+   */
+  public boolean offer(T item, double score) {
+    if (Double.isInfinite(score) || Double.isNaN(score))
       throw new IllegalArgumentException();
-    offers++;
-    if (bestItem == null || score > bestScore) {
+    boolean newArgmax = offers == 0 || score > bestScore;
+    if (newArgmax) {
       bestItem = item;
       bestScore = score;
+      bestIndex = offers;
     }
+    offers++;
+    return newArgmax;
+  }
+
+  /** How many offers PRECEDED the best one, or the 0-based index of the best item. */
+  public int getBestIndex() {
+    return bestIndex;
   }
 
   public int numOffers() {
@@ -25,5 +43,9 @@ public class ArgMax<T> implements Serializable {
 
   public T get() {
     return bestItem;
+  }
+
+  public double getBestScore() {
+    return bestScore;
   }
 }
