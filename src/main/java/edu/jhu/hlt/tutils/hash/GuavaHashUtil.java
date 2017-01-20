6c887594a9b8152e638dcdf26f29978116b6d60e
Travis Wolfe <twolfe18@gmail.com>
2017-01-20 11:35:05 -0500

    fixed typo in GuavaHashUtil.goodFastHash

--- src/main/java/edu/jhu/hlt/tutils/hash/GuavaHashUtil.java
+++ src/main/java/edu/jhu/hlt/tutils/hash/GuavaHashUtil.java
@@ -1,5 +1,6 @@
 package edu.jhu.hlt.tutils.hash;
 
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import com.google.common.hash.HashFunction;
@@ -27,7 +28,7 @@ public class GuavaHashUtil {
     int hashFunctionsNeeded = (bits + 127) / 128;
     HashFunction[] hashFunctions = new HashFunction[hashFunctionsNeeded];
 //    hashFunctions[0] = Murmur3_128Holder.GOOD_FAST_HASH_FUNCTION_128;
-    hashFunctions[0] = Hashing.murmur3_32(seed);
+    hashFunctions[0] = Hashing.murmur3_128(seed);
 //    int seed = GOOD_FAST_HASH_SEED;
     for (int i = 1; i < hashFunctionsNeeded; i++) {
       seed += 1500450271; // a prime; shouldn't matter
@@ -44,5 +45,25 @@ public class GuavaHashUtil {
     if (bits <= 0)
       throw new IllegalArgumentException("Number of bits must be positive");
     return (bits + 31) & ~31;
+  }
+  
+  
+  public static void main(String[] args) {
+    int nhash = 10;
+    int logb = 20;
+    
+    int neededBits = 8 * nhash * logb;
+    System.out.println("needed bits: " + neededBits);
+    System.out.println("needed bytes: " + nhash*logb);
+    HashFunction hf1 = Hashing.goodFastHash(neededBits);
+    HashFunction hf2 = goodFastHash(neededBits, 9001);
+    
+    Charset cs = Charset.forName("UTF8");
+    byte[] b1 = hf1.hashString("hello", cs).asBytes();
+    byte[] b2 = hf2.hashString("hello", cs).asBytes();
+    System.out.println(b1.length);
+    System.out.println(b2.length);
+    System.out.println(Arrays.toString(b1));
+    System.out.println(Arrays.toString(b2));
   }
 }
